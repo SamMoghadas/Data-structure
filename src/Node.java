4bfad90b00f3552b1738dd089baa4896fb3ac28c
SamMoghadas <sammoghadas2121@gmail.com>
2025-02-08 19:17:22 +0330

    init commit

---
+++ src/Node.java
@@ -0,0 +1,9 @@
+public class Node {     // Nodes have 2 part   :
+    int data ;          // first is data, the main thing
+    Node next ;         // second is the pointer
+
+    public Node (int data) {
+        this.data = data ;
+        this.next = null ;
+    }
+}
